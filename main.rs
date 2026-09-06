1ffe7e1395a2fbcc304980f8a6cdaf05a1801cb3
Michal Pokorný <michal.pokorny0@gmail.com>
2026-09-06 18:17:09 +0200

    05

--- main.rs
+++ main.rs
@@ -2,22 +2,24 @@ use std::cell::RefCell;
 use std::io::{self, BufRead};
 use std::rc::Rc;
 
-macro_rules! sum {
-    ($($x:expr),*) => {
-        0 $(+$x)*
-    };
+fn longer<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        a
+    }
+    else {
+        b
+    }
 }
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let s: Vec<String> = stdin.lock().lines().collect::<Result<_,_>>()?;
     //let nums: Vec<i32> = s[0].split_whitespace().map(|s| s.parse().unwrap()).collect();
-    let n = s[0].parse::<i32>().unwrap();
-    let raw: *const i32 = &n;
 
-    unsafe {
-        println!("{}", *raw);
-    }
+    let a = &s[0];
+    let b = &s[1];
+
+    println!("{}", longer(a,b ));
 
     Ok(())
 }
