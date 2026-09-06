dc7fb5b0844bee8e74b4b0f54cdeb4b0749c5309
Michal Pokorný <michal.pokorny0@gmail.com>
2026-09-06 17:57:41 +0200

    01

--- main.rs
+++ main.rs
@@ -1,14 +1,17 @@
 use std::io::{self, BufRead};
 use std::rc::Rc;
 
-fn main() {
+fn main() -> io::Result<()> {
     let stdin = io::stdin();
-    let mut line = String::new();
-    stdin.lock().read_line(&mut line).unwrap();
-    let nums: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let s: Vec<String> = stdin.lock().lines().collect::<Result<_,_>>()?;
+    let nums: Vec<i32> = s[0].split_whitespace().map(|s| s.parse().unwrap()).collect();
 
-    // TODO: move `nums` into an Rc.
-    // TODO: create two more handles to it.
-    // TODO: print "count: " and the strong count.
-    // TODO: print "sum: " and the sum of the shared vec.
+    let vec = Rc::new(nums);
+    let a = Rc::clone(&vec);
+    let b = Rc::clone(&vec);
+
+    println!("count: {}", Rc::strong_count(&a));
+    println!("sum: {}", a.iter().sum::<i32>());
+
+    Ok(())
 }
